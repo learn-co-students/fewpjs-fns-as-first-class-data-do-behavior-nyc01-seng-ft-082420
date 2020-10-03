1b66d6ad71a3f8731891d60ae85d012fcbb3a2a6
AB <austinbuhler@gmail.com>
2020-10-03 17:21:39 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  if (time < '12:00') {
+    return "Good Morning"
+  } else if (time > '12:00' && time < '17:00') {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.querySelector('#greeting').textContent = string
+}
