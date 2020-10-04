434435adfc4ab81d9577cf263aace4f4e4ca6f8c
zhengjianliu <comezheng@gmail.com>
2020-10-04 18:53:39 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  const hr = parseInt(time, 10);
+  if(hr < 12){
+    return "Good Morning"
+  }else if (hr > 12 && hr < 17) {
+    return "Good Afternoon"
+  }else{
+    return "Good Evening"
+  }
+}
+
+function displayMessage(msg) {
+  document.getElementById('greeting').innerText = msg;
+}
